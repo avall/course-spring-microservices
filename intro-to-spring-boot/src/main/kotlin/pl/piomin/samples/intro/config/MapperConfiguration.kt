ecc1e9e936399e72881b0deb4c1e294dd0cf60e5
Alex Vall Mainou <alex.vall@financefox.com>
2021-12-17 11:54:49 +0100

    Simple sample

---
+++ intro-to-spring-boot/src/main/kotlin/pl/piomin/samples/intro/config/MapperConfiguration.kt
@@ -0,0 +1,21 @@
+package pl.piomin.samples.intro.config
+
+//import com.fasterxml.jackson.annotation.JsonInclude
+//import com.fasterxml.jackson.databind.DeserializationFeature
+//import com.fasterxml.jackson.databind.ObjectMapper
+//import com.fasterxml.jackson.databind.PropertyNamingStrategy
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+class ObjectMapperConfiguration {
+//    @Bean
+//    fun objectMapper() = ObjectMapper().apply {
+//        findAndRegisterModules()
+//        registerModule(JavaTimeModule())
+//        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+//        setSerializationInclusion(JsonInclude.Include.NON_NULL)
+//        propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+//    }
+}
--- intro-to-spring-boot/src/main/kotlin/pl/piomin/samples/intro/domain/Person.kt
+++ intro-to-spring-boot/src/main/kotlin/pl/piomin/samples/intro/domain/Person.kt
@@ -3,4 +3,6 @@ package pl.piomin.samples.intro.domain
 data class Person(val id: Int,
                   val firstName: String,
                   val lastName: String,
-                  var age: Int)
+                  var age: Int,
+                  val phone:String?=null
+)
